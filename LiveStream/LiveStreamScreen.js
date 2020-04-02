61fe51669f9fb5e6486fe0e0d5df2c2df4205333
Jeremy Krinsley <krinsley@gmail.com>
2020-04-01 21:03:06 -0400

    mv livestream to common

---
+++ LiveStream/LiveStreamScreen.js
@@ -0,0 +1,45 @@
+import React, { useMemo, useRef, useResource } from "react";
+import * as THREE from "three";
+import Hls from 'hls.js';
+
+export default function LiveStreamScreen({ src, sizeX, sizeY, play, position }) {
+
+  // Create video element
+  const [material, geometry] = useMemo(() => {
+    const video = document.createElement("video");
+    document.body.appendChild(video);
+    // Add support for HLS stream 
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(src);
+      hls.attachMedia(video);
+      // play video
+      if (play) {
+        video.play();
+      }
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+      // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+      // This is using the built-in support of the plain video element, without using hls.js.
+      video.src = src;
+      video.addEventListener('canplay', function () {
+        if (play) {
+          video.play();
+        }
+      });
+    }
+    // create material from video texture
+    let texture = new THREE.VideoTexture(video);
+    texture.minFilter = THREE.LinearFilter;
+    texture.format = THREE.RGBFormat;
+    let material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+    let geometry = new THREE.PlaneBufferGeometry(sizeX, sizeY);
+    return [material, geometry];
+  })
+
+  return (
+    <mesh material={material} position={position}>
+      <bufferGeometry attach="geometry" {...geometry} />
+    </mesh>
+  );
+}
