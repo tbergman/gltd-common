66a800a9450bf05c9532313d1d8d7041e2c9880e
Jeremy Krinsley <krinsley@gmail.com>
2020-05-20 20:40:27 -0400

    cleanup tron2

--- Materials/TronMaterial2.js
+++ Materials/TronMaterial2.js
@@ -2,82 +2,38 @@
 import simpleVertex from '!raw-loader!glslify-loader!../Shaders/simpleVertex.glsl';
 /* eslint import/no-webpack-loader-syntax: off */
 import tronFragmentShader from '!raw-loader!glslify-loader!../Shaders/tronFragment2.glsl';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useMemo } from 'react';
 import { useFrame, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
 import { hexToRgb } from '../Utils/colors';
 
-export default function TronMaterial2({ materialRef, side, color }) {
-	color = color || 0xffffff;
+export default function TronMaterial2({ materialRef, bpm, side, colorOffset, ...props }) {
+	colorOffset = colorOffset ? colorOffset : new THREE.Vector3(0., .9, .6);
 	materialRef = materialRef ? materialRef : useRef().current;
 	const { clock, size } = useThree();
-	const uniforms = useRef();
-	useEffect(() => {
-		const baseColor = hexToRgb(color.toString(16));
-		uniforms.current = {
+	const uniforms = useMemo(() => {
+		return {
+			colorOffset: new THREE.Uniform(colorOffset),
 			uTime: { value: 0 },
 			uResolution: { value: new THREE.Vector2(size.width, size.length) },
-			uBPM: { value: 120 },
-			uBaseColor: { value: new THREE.Vector3(baseColor.r, baseColor.g, baseColor.g) },
+			uBPM: { value: props.bpm ? props.pbm : 120 },
 		}
-	}, []);
-
-	useFrame(() => {
-		if (!uniforms.current.uTime) return; // avoid re-initialization async issues (e.g. if tiling)
-		uniforms.current.uTime.value = clock.oldTime;
 	});
 
-	// useEffect(() => {
-	// 	if (uniforms.current.uBPM) uniforms.current.uBPM.value = bpm;
-	// }, [bpm])
+	useFrame(() => {
+		if (!uniforms.uTime) return;
+		uniforms.uTime.value = clock.oldTime;
+	});
+
+	useEffect(() => {
+		if (uniforms.uBPM) uniforms.uBPM.value = bpm;
+	}, [bpm])
 
 	return <shaderMaterial
 		ref={materialRef}
-		uniforms={uniforms.current}
+		uniforms={uniforms}
 		side={side}
 		vertexShader={simpleVertex}
 		fragmentShader={tronFragmentShader}
 	/>;
 }
-
-// /* eslint import/no-webpack-loader-syntax: off */
-// import simpleVertex from '!raw-loader!glslify-loader!../Shaders/simpleVertex.glsl';
-// /* eslint import/no-webpack-loader-syntax: off */
-// import tronFragmentShader from '!raw-loader!glslify-loader!../Shaders/tronFragment2.glsl';
-// import React, { useEffect, useRef } from 'react';
-// import { useFrame, useThree } from 'react-three-fiber';
-// import * as THREE from 'three';
-// import { hexToRgb } from '../Utils/colors';
-
-// export default function TronMaterial2({ materialRef, color, ...props }) {
-// 	color = color || 0xffffff;
-// 	materialRef = materialRef ? materialRef : useRef().current;
-// 	const { clock, size } = useThree();
-// 	const uniforms = useRef();
-// 	useEffect(() => {
-// 		const baseColor = hexToRgb(color.toString(16));
-// 		uniforms.current = {
-// 			uTime: { value: 0 },
-// 			uResolution: { value: new THREE.Vector2(size.width, size.length) },
-// 			uBPM: { value: 120 },
-// 			uBaseColor: { value: new THREE.Vector3(baseColor.r, baseColor.g, baseColor.g) },
-// 		}
-// 	}, []);
-
-// 	useFrame(() => {
-// 		if (!uniforms.current.uTime) return; // avoid re-initialization async issues (e.g. if tiling)
-// 		uniforms.current.uTime.value = clock.oldTime;
-// 	});
-
-// 	// useEffect(() => {
-// 	// 	if (uniforms.current.uBPM) uniforms.current.uBPM.value = bpm;
-// 	// }, [bpm])
-
-// 	return <shaderMaterial
-// 		ref={materialRef}
-// 		uniforms={uniforms.current}
-// 		side={props.side ? props.side : THREE.FrontSide}
-// 		vertexShader={simpleVertex}
-// 		fragmentShader={tronFragmentShader}
-// 	/>;
-// }
