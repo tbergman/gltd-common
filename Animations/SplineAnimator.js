ec9a8bba7e0a015723727cc3ac0fec30463442c0
Jeremy Krinsley <krinsley@gmail.com>
2020-07-15 00:11:47 -0400

    mv both trajectory funcs from jav to common

--- Animations/SplineAnimator.js
+++ Animations/SplineAnimator.js
@@ -1,5 +1,6 @@
 import React, {useMemo, useRef, useEffect} from 'react';
 import * as THREE from 'three';
+import {useFrame} from 'react-three-fiber';
 
 export function useObjectAlongTubeGeometry({ object, tubeGeometry, ...props }) {
     const [normal, binormal] = useMemo(() => {
@@ -12,12 +13,7 @@ export function useObjectAlongTubeGeometry({ object, tubeGeometry, ...props }) {
     const offset = useRef();
     const delta = useRef();
     const speed = useRef();
-    function getCurTrajectory({
-        tubeGeometry,
-        offset,
-        delta,
-        t,
-    }) {
+    function getCurTrajectory(t) {
         const pos = tubeGeometry.parameters.path.getPointAt(t);
         offset.current += delta.current;
         // interpolation
@@ -33,12 +29,25 @@ export function useObjectAlongTubeGeometry({ object, tubeGeometry, ...props }) {
         pos.add(normal.clone());
         return { pos, dir };
     }
+    function updateCurTrajectory ({t, pos, dir}) {
+        object.position.copy(pos);
+        // Using arclength for stablization in look ahead.
+        const lookAt = tubeGeometry.parameters.path.getPointAt((t + 30 / tubeGeometry.parameters.path.getLength()) % 1);
+        // Camera Orientation 2 - up orientation via normal
+        lookAt.copy(pos).add(dir);
+        object.matrix.lookAt(object.position, lookAt, normal);
+        object.rotation.setFromRotationMatrix(object.matrix);
+        // car.rotation.z += Math.PI / 12; // TODO added code - can it be baked into matrix rotation?
+    }
+    
     useEffect(() => {
         if (!speed.current) speed.current = 20;
         if (!delta.current) delta.current = .005;
         if (!offset.current) offset.current = 0;
     })
-
+    // useFrame(() => {
+    //     updateSpeed();
+    // })
     return {
         offset,
         delta,
@@ -46,7 +55,7 @@ export function useObjectAlongTubeGeometry({ object, tubeGeometry, ...props }) {
         normal,
         binormal,
         getCurTrajectory,
-
+        updateCurTrajectory,
     }
 }
 
@@ -54,13 +63,3 @@ export function useObjectAlongTubeGeometry({ object, tubeGeometry, ...props }) {
 
 
 
-export const updateCurTrajectory = ({t, pos, dir, normal, object, tubeGeometry}) => {
-    object.position.copy(pos);
-    // Using arclength for stablization in look ahead.
-    const lookAt = tubeGeometry.parameters.path.getPointAt((t + 30 / tubeGeometry.parameters.path.getLength()) % 1);
-    // Camera Orientation 2 - up orientation via normal
-    lookAt.copy(pos).add(dir);
-    object.matrix.lookAt(object.position, lookAt, normal);
-    object.rotation.setFromRotationMatrix(object.matrix);
-    // car.rotation.z += Math.PI / 12; // TODO added code - can it be baked into matrix rotation?
-}
