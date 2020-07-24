36c647660c5350963f98d04c8712dc0510d8bee8
Jeremy Krinsley <krinsley@gmail.com>
2020-07-24 00:09:48 -0400

    tweaks to sunflare and polished speckled envmap

--- Materials/PolishedSpeckledMarbleTop.js
+++ Materials/PolishedSpeckledMarbleTop.js
@@ -6,7 +6,7 @@ import metallic from "../assets/textures/marble-speckled-bl/marble-speckled-meta
 import normal from "../assets/textures/marble-speckled-bl/marble-speckled-normal.png";
 import roughness from "../assets/textures/marble-speckled-bl/marble-speckled-roughness.png";
 
-export default function PolishedSpeckledMarbleTop({ materialRef, ...props }) {
+export default function PolishedSpeckledMarbleTop({ materialRef, useEnvMap=true, ...props }) {
     // source https://freepbr.com/materials/polished-speckled-marble-top-pbr-material/
     const [albedoMap, metallicMap, normalMap, roughnessMap, envMap] = useMemo(() => {
         const textureLoader = new THREE.TextureLoader();
@@ -18,6 +18,7 @@ export default function PolishedSpeckledMarbleTop({ materialRef, ...props }) {
         const textureMaps = [albedoMap, metallicMap, normalMap, roughnessMap, envMap];
         return tileTextureMaps(textureMaps, props);
     });
+    console.log("USE ENVMAP!", props.useEnvMap)
     return <meshStandardMaterial
         ref={materialRef}
         lights
@@ -26,9 +27,11 @@ export default function PolishedSpeckledMarbleTop({ materialRef, ...props }) {
         map={albedoMap}
         metallicMap={metallicMap}
         normalMap={normalMap}
+        metalness={1}
         roughnessMap={roughnessMap}
+        roughness={1}
         skinning={props.skinning || false}
-        envMap={envMap}
+        envMap={useEnvMap ? envMap : undefined}
         side={props.side ? props.sid : THREE.FrontSide}
         {...props}
     />;
--- Shaders/sunflareFragment.glsl
+++ Shaders/sunflareFragment.glsl
@@ -1,40 +1,27 @@
 // original work from: https://www.shadertoy.com/view/MtGGzW
 varying vec2 vUv;
-
-// uniform vec3 colorOffset;
 uniform float uTime;
 
 void main() {
 
-	vec2 uv = vUv - 0.5;
+	vec2 uv = vUv - 0.1;
 	
     float gt = uTime / 1000.;
     float algo_g = 0.0;
     gt = 2.0 * sin(gt * 0.1);
 	
-    uv.y += 0.3;
+    uv.y += 5.;
 	    
 	algo_g += distance(cos(uv.x) * (1.0 - uv.y) * (uv.x * sin(gt)) + gt * gt, (uv.x * sin(gt)) * gt * (1.0 - uv.y) + 6.91 * sin(gt));		       
 	algo_g *= distance(8.05 * uv.x * (uv.y * sin(gt)) + sin(gt) + (uv.x * tan(gt)), (uv.y * sin(gt)) + gt * sin(uv.x) + uv.y + (1.0 - uv.y));
-	algo_g += (sin(gt) * (1.0 - uv.y) + (1.0 - uv.y) * 2.76 + cos(uv.x));
-	algo_g *= mod(sin(uv.x) * (1.0 - uv.y) * sin(uv.x) + sin(uv.x) + uv.y, (1.0 - uv.x) + (uv.x * tan(gt)) * 0.60 * 0.0 + cos(uv.x));	
+	//algo_g += (sin(gt) * (1.0 - uv.y) + (1.0 - uv.y) * 2.76 + cos(uv.x));
+	algo_g *= mod(sin(uv.x) * (1.0 - uv.y) * sin(uv.x) + sin(uv.x) + uv.y, (1.0 - uv.x) + (uv.x * tan(gt)) * 0.80  + cos(uv.x));	
     
 	algo_g = (1.0 - algo_g); 
     
     algo_g = clamp(algo_g, 0.0, 1.0);
 	
-	gl_FragColor = vec4(algo_g, algo_g, algo_g, .5);
-
-    //////
-
-//   vec2 wavyLines = vec2(sin(vuv.x * mod(uTime, 10000.) ), sin(vUv.y * mod(uTime, 10000.)));
-  
-//   float x = sin(vUv.x / vUv.y) + colorOffset.g;
-//   float y = wavyLines.y + colorOffset.g;
-//   float z = colorOffset.b;
-  
-  
-//   gl_FragColor = vec4(x, y, z, 1.0);
+	gl_FragColor = vec4(0.12, 0., algo_g / 2.0, 0.3);
 }
 
 
